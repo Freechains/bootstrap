996a6147f55c7ae75775372a4eb06dc6ab408e59
fsantanna <francisco.santanna@gmail.com>
2020-06-10 18:07:01 -0300

    (>) update, Store, -Chain

--- src/main/kotlin/org/freechains/bootstrap/Bootstrap.kt
+++ src/main/kotlin/org/freechains/bootstrap/Bootstrap.kt
@@ -13,48 +13,34 @@ import java.net.Socket
 import kotlin.concurrent.thread
 
 @Serializable
-data class Chain (
-    val name : String,
-    val key  : HKey?
-)
-
-@Serializable
-data class Id (
-    val nick : String,
-    val pub  : HKey
-)
-
-@Serializable
-data class DBootstrap (
+data class Store (
     val peers  : MutableList<String>,
-    val chains : MutableList<Chain>,
-    val ids    : MutableList<Id>,
-    val cts    : MutableList<Id>
+    val chains : MutableList<Pair<String,HKey?>>
 )
 
-class Bootstrap (root: String, chain: String, port: Int) {
-    val cbs: MutableSet<(DBootstrap)->Unit> = mutableSetOf()
+class Bootstrap (path: String, chain: String, port: Int = PORT_8330) {
+    val cbs: MutableSet<(Store)->Unit> = mutableSetOf()
 
     private val chain = chain
-    private val path  = root + "/" + chain + "/bootstrap.json"
-    private var data = DBootstrap(mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf())
+    private var data  = Store(mutableListOf(), mutableListOf())
 
     private fun json2data (json: String) {
         @OptIn(UnstableDefault::class)
-        this.data = Json(JsonConfiguration(prettyPrint=true)).parse(DBootstrap.serializer(), json)
+        this.data = Json(JsonConfiguration(prettyPrint=true)).parse(Store.serializer(), json)
     }
     private fun data2json (): String {
         @OptIn(UnstableDefault::class)
-        return Json(JsonConfiguration(prettyPrint=true)).stringify(DBootstrap.serializer(), this.data)
+        return Json(JsonConfiguration(prettyPrint=true)).stringify(Store.serializer(), this.data)
     }
 
     init {
-        val file = File(this.path)
+        val file = File(path)
         if (!file.exists()) {
-            File(this.path).writeText(this.data2json())
+            File(path).writeText(this.data2json())
         } else {
             this.json2data(file.readText())
         }
+        this.update()
 
         // background listen
         thread {
@@ -64,22 +50,29 @@ class Bootstrap (root: String, chain: String, port: Int) {
             writer.writeLineX("$PRE chain $chain listen")
             while (true) {
                 reader.readLineX()
-                val head = main_cli_assert(arrayOf("chain", this.chain, "heads", "all")).let {
-                    val heads = it.split(' ')
-                    assert_(heads.size == 1)
-                    heads[0]
-                }
-                val pay = main_cli_assert(arrayOf("chain", this.chain, "get", "payload", head))
-                synchronized (this) {
-                    this.json2data(pay)
-                    this.cbs.forEach { it(this.data) }
-                }
+                this.update()
+            }
+        }
+    }
+
+    // update data and invoke callbacks
+    fun update () {
+        thread {
+            val head = main_cli_assert(arrayOf("chain", chain, "heads", "all")).let {
+                val heads = it.split(' ')
+                assert_(heads.size == 1)
+                heads[0]
+            }
+            val pay = main_cli_assert(arrayOf("chain", chain, "get", "payload", head))
+            synchronized (this) {
+                this.json2data(pay)
+                this.cbs.forEach { it(this.data) }
             }
         }
     }
 
     @Synchronized
-    fun write (f: (DBootstrap)->Unit) {
+    fun write (f: (Store)->Unit) {
         f(this.data)
         val json = this.data2json()
         thread {
@@ -88,7 +81,7 @@ class Bootstrap (root: String, chain: String, port: Int) {
     }
 
     @Synchronized
-    fun <T> read (f: (DBootstrap)->T) : T {
+    fun <T> read (f: (Store)->T) : T {
         return f(this.data)
     }
 }

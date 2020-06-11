3d8b49a7d35bf5ba5cd5a3aff665e95ecff90c49
fsantanna <francisco.santanna@gmail.com>
2020-06-11 19:58:26 -0300

    (>) main functions

--- src/main/kotlin/org/freechains/bootstrap/Main.kt
+++ src/main/kotlin/org/freechains/bootstrap/Main.kt
@@ -17,7 +17,7 @@ Usage:
 
 Options:
     --help          displays this help
-    --version       displays version information
+    --version       displays software version
     --port          port to connect [default: $PORT_8330]
 
 More Information:
@@ -28,54 +28,43 @@ More Information:
 """
 
 fun main (args: Array<String>) {
-    main_bootstrap(args).let { (ok,msg) ->
-        if (ok) {
-            if (msg.isNotEmpty()) {
-                println(msg)
-            }
-        } else {
-            System.err.println(msg)
-            exitProcess(1)
-        }
-    }
+    main_ { main_bootstrap(args) }
+}
+
+fun main_bootstrap_assert (args: Array<String>) : String {
+    return main_assert_ { main_bootstrap(args) }
 }
 
 fun main_bootstrap (args: Array<String>) : Pair<Boolean,String> {
-    return catch_all("freechains-bootstrap ${args.joinToString(" ")}") {
-        val (cmds, opts) = args.cmds_opts()
-        val port = if (opts.containsKey("--port")) opts["--port"]!!.toInt() else PORT_8330
+    return main_catch_("freechains-bootstrap", VERSION, help, args) { cmds, opts ->
+        val port = opts["--port"]?.toInt() ?: PORT_8330
         val port_ = "--port=$port"
 
         @Suppress("UNREACHABLE_CODE")
-        when {
-            opts.containsKey("--help") -> Pair(true, help)
-            else -> {
-                val path = main_host_assert(arrayOf(port_, "path"))
-                when (cmds[0]) {
-                    "start" -> {
-                        assert_(cmds.size == 1)
-                        val chain = File(path)
-                            .list()!!
-                            .first { f -> f.endsWith(".bootstrap") }
-                            .dropLast(".bootstrap".length)
-                        Chain(path, chain, port)
-                        while (true);
-                        Pair(true, "")
-                    }
-                    "init" -> {
-                        assert_(cmds.size == 4)
-                        val chain = cmds[2]
-                        val host = "--host=localhost:$port"
-                        assert_(chain.startsWith("\$bootstrap."))
-                        main_cli(arrayOf(host, "chains", "join", chain, cmds[3]))
-                        Chain(path, chain, port)
-                        main_cli_assert(arrayOf(host, "peer", cmds[1], "recv", chain))
-                        while (true);
-                        Pair(true, "")
-                    }
-                    else -> Pair(false, "!")
-                }
+        val path = main_host_assert(arrayOf(port_, "path"))
+        when (cmds[0]) {
+            "start" -> {
+                assert_(cmds.size == 1)
+                val chain = File(path)
+                    .list()!!
+                    .first { f -> f.endsWith(".bootstrap") }
+                    .dropLast(".bootstrap".length)
+                Chain(path, chain, port)
+                while (true);
+                Pair(true, "")
             }
+            "init" -> {
+                assert_(cmds.size == 4)
+                val chain = cmds[2]
+                val host = "--host=localhost:$port"
+                assert_(chain.startsWith("\$bootstrap."))
+                main_cli(arrayOf(host, "chains", "join", chain, cmds[3]))
+                Chain(path, chain, port)
+                main_cli_assert(arrayOf(host, "peer", cmds[1], "recv", chain))
+                while (true);
+                Pair(true, "")
+            }
+            else -> Pair(false, "!")
         }
     }
 }

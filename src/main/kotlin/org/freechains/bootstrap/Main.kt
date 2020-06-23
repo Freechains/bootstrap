cec15ac63f9e89d2fb5ca1ed328a0bf53c3d4ca6
fsantanna <francisco.santanna@gmail.com>
2020-06-23 10:48:10 -0300

    (>) remote requires chain to be previously created

--- src/main/kotlin/org/freechains/bootstrap/Main.kt
+++ src/main/kotlin/org/freechains/bootstrap/Main.kt
@@ -13,7 +13,7 @@ freechains-bootstrap $VERSION
 
 Usage:
     freechains-bootstrap local <chain>
-    freechains-bootstrap remote <peer> <chain> <shared>
+    freechains-bootstrap remote <peer> <chain>
 
 Options:
     --help          displays this help
@@ -40,21 +40,19 @@ fun main_bootstrap (args: Array<String>) : Pair<Boolean,String> {
         val port = opts["--port"]?.toInt() ?: PORT_8330
         val port_ = "--port=$port"
 
-        val path = main_host_assert(arrayOf(port_, "path"))
         @Suppress("UNREACHABLE_CODE")
         when (cmds[0]) {
             "local" -> {
                 assert_(cmds.size == 2) { "invalid number of arguments" }
-                Chain(path, cmds[1], port)
+                Chain(cmds[1], port)
                 while (true);
                 Pair(true, "")
             }
             "remote" -> {
-                assert_(cmds.size == 4) { "invalid number of arguments" }
+                assert_(cmds.size == 3) { "invalid number of arguments" }
                 val chain = cmds[2]
                 assert_(chain.startsWith("\$bootstrap.")) { "invalid chain name" }
-                main_cli(arrayOf(port_, "chains", "join", chain, cmds[3]))
-                Chain(path, chain, port)
+                Chain(chain, port)
                 main_cli_assert(arrayOf(port_, "peer", cmds[1], "recv", chain))
                 while (true);
                 Pair(true, "")
